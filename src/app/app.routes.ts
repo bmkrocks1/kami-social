2e81a7628dacdf9e584da6a58e2dc37c499492a4
Billie Ko <bmkrocks@gmail.com>
2024-04-25 13:58:26 +0800

    fix route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -32,36 +32,56 @@ export const routes: Routes = [
   {
     path: 'albums',
     data: { breadcrumb: 'Albums' },
-    loadComponent: () =>
-      import('./components/albums/albums.component').then(
-        (e) => e.AlbumsComponent
-      ),
-  },
-  {
-    path: 'albums/:id',
-    loadComponent: () =>
-      import('./components/album/album.component').then(
-        (e) => e.AlbumComponent
-      ),
+    children: [
+      {
+        path: ':id',
+        data: { breadcrumb: ':id' },
+        loadComponent: () =>
+          import('./components/albums/albums.component').then(
+            (e) => e.AlbumsComponent
+          ),
+      },
+      {
+        path: '',
+        data: { breadcrumb: null },
+        loadComponent: () =>
+          import('./components/album/album.component').then(
+            (e) => e.AlbumComponent
+          ),
+      },
+    ],
   },
   {
     path: 'photos',
     data: { breadcrumb: 'Photos' },
-    loadComponent: () =>
-      import('./components/photos/photos.component').then(
-        (e) => e.PhotosComponent
-      ),
-  },
-  {
-    path: 'photos/:id',
-    loadComponent: () =>
-      import('./components/photo/photo.component').then(
-        (e) => e.PhotoComponent
-      ),
+
+    children: [
+      {
+        path: ':id',
+        data: { breadcrumb: ':id' },
+        loadComponent: () =>
+          import('./components/photo/photo.component').then(
+            (e) => e.PhotoComponent
+          ),
+      },
+      {
+        path: '',
+        data: { breadcrumb: null },
+        loadComponent: () =>
+          import('./components/photos/photos.component').then(
+            (e) => e.PhotosComponent
+          ),
+      },
+    ],
   },
   {
     path: 'user/:id',
     loadComponent: () =>
       import('./components/user/user.component').then((e) => e.UserComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/home/home.component').then((e) => e.HomeComponent),
   },
 ];
